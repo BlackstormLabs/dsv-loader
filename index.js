eb90ca6a63002a83305a2e8d5dec2f62fa175c96
yofreke <sparkyjoe94@aim.com>
2017-06-12 17:58:20 -0700

    parse numbers

--- index.js
+++ index.js
@@ -4,16 +4,37 @@
 var loaderUtils = require('loader-utils');
 var dsvFormat = require('d3-dsv').dsvFormat;
 
+
+var rowFormat = function(rowValues, rowIndex, columnNames) {
+  var result = {};
+  for (var i = 0; i < columnNames.length; i++) {
+    var columnName = columnNames[i];
+    var value = rowValues[columnName];
+    if (
+      typeof value === 'string'
+      && value.match(/^-?[0-9\.]+$/)
+    ) {
+      result[columnName] = JSON.parse(value);
+    } else {
+      result[columnName] = value;
+    }
+  }
+  return result;
+};
+
+
 module.exports = function(text) {
   this.cacheable();
-  
-  var query = loaderUtils.parseQuery(this.query),
-      delimiter = query.delimiter || ',',
-      dsv = dsvFormat(delimiter),
-      rows = query.rows,
-      res = rows ? dsv.parseRows(text) : dsv.parse(text);
 
-  return 'var res = ' + JSON.stringify(res) + ';' +
+  var query = loaderUtils.parseQuery(this.query);
+  var delimiter = query.delimiter || ',';
+  var dsv = dsvFormat(delimiter);
+  var rows = query.rows;
+  var res = rows ? dsv.parseRows(text, rowFormat) : dsv.parse(text, rowFormat);
+
+  return (
+    'var res = ' + JSON.stringify(res) + ';' +
     'res.columns = ' + JSON.stringify(res.columns) + ';' +
-    'module.exports = res;';
+    'module.exports = res;'
+  );
 }
